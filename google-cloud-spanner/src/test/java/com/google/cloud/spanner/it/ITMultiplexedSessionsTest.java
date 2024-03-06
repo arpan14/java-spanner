421d149c287f079b6b3d5796e7ddcc2858922d96
Arpan Mishra <arpanmishra@google.com>
2024-03-06 17:36:39 +0530

    chore: fix mux integration test.

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITMultiplexedSessionsTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITMultiplexedSessionsTest.java
@@ -18,17 +18,28 @@ package com.google.cloud.spanner.it;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.cloud.spanner.Database;
+import com.google.cloud.spanner.DatabaseAdminClient;
 import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
 import com.google.cloud.spanner.IntegrationTestEnv;
 import com.google.cloud.spanner.Key;
 import com.google.cloud.spanner.Mutation;
 import com.google.cloud.spanner.ParallelIntegrationTest;
+import com.google.cloud.spanner.SessionPoolOptions;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
 import com.google.cloud.spanner.Struct;
 import com.google.cloud.spanner.TimestampBound;
 import com.google.cloud.spanner.connection.ConnectionOptions;
+import com.google.common.collect.ImmutableList;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -46,23 +57,46 @@ import org.junit.runners.JUnit4;
 @Category(ParallelIntegrationTest.class)
 @RunWith(JUnit4.class)
 public class ITMultiplexedSessionsTest {
+  private static final String INSTANCE_ID = "arpanmishra-dev-span";
+  private static final String SERVER_URL = "https://staging-wrenchworks.sandbox.googleapis.com";
+  private static DatabaseAdminClient databaseAdminClient;
+
   @ClassRule public static IntegrationTestEnv env = new IntegrationTestEnv();
+  private static final AtomicInteger dbSeq = new AtomicInteger();
+  private static final int dbPrefix = new Random().nextInt(Integer.MAX_VALUE);
   private static final String TABLE_NAME = "TestTable";
   private static final List<String> ALL_COLUMNS = Arrays.asList("Key", "StringValue");
   private static DatabaseClient client;
+  private static Spanner spanner;
+  private static List<String> databaseIds = new ArrayList<>();
 
   @BeforeClass
-  public static void setUpDatabase() {
-    Database googleStandardSQLDatabase =
-        env.getTestHelper()
-            .createTestDatabase(
+  public static void setUpDatabase() throws Exception {
+    final String databaseId = getUniqueDatabaseId();
+    SpannerOptions options =
+        SpannerOptions.newBuilder()
+            .setSessionPoolOption(
+                SessionPoolOptions.newBuilder()
+                    .setUseMultiplexedSession(true).build())
+            .setHost(SERVER_URL)
+            .build();
+    spanner = options.getService();
+    databaseAdminClient = spanner.getDatabaseAdminClient();
+    final Database database =
+        databaseAdminClient
+            .createDatabase(INSTANCE_ID, databaseId, ImmutableList.of(
                 "CREATE TABLE TestTable ("
                     + "  key                STRING(MAX) NOT NULL,"
                     + "  stringvalue        STRING(MAX),"
                     + ") PRIMARY KEY (key)",
                 "CREATE INDEX TestTableByValue ON TestTable(stringvalue)",
-                "CREATE INDEX TestTableByValueDesc ON TestTable(stringvalue DESC)");
-    client = env.getTestHelper().getDatabaseClient(googleStandardSQLDatabase);
+                "CREATE INDEX TestTableByValueDesc ON TestTable(stringvalue DESC)"
+            ))
+            .get(Duration.ofMinutes(2).toNanos(), TimeUnit.NANOSECONDS);
+
+    databaseIds.add(databaseId);
+
+    client = spanner.getDatabaseClient(database.getId());
 
     // Includes k0..k14.  Note that strings k{10,14} sort between k1 and k2.
     List<Mutation> mutations = new ArrayList<>();
@@ -80,7 +114,8 @@ public class ITMultiplexedSessionsTest {
 
   @AfterClass
   public static void teardown() {
-    ConnectionOptions.closeSpanner();
+    cleanUp();
+    spanner.close();
   }
 
   @Test
@@ -94,5 +129,21 @@ public class ITMultiplexedSessionsTest {
     // Ensure that the Struct implementation supports equality properly.
     assertThat(row)
         .isEqualTo(Struct.newBuilder().set("key").to("k1").set("stringvalue").to("v1").build());
+  }
+
+  public static String getUniqueDatabaseId() {
+    return String.format("testdb_%d_%04d", dbPrefix, dbSeq.incrementAndGet());
+  }
+
+  public static void cleanUp() {
+    // Drop all the databases we created explicitly.
+    int numDropped = 0;
+    for (String databaseId : databaseIds) {
+      try {
+        spanner.getDatabaseAdminClient().dropDatabase(INSTANCE_ID, databaseId);
+        ++numDropped;
+      } catch (Throwable e) {
+      }
+    }
   }
 }
