d2abd232208145168c5cb958de64e4d46139d992
Arpan Mishra <arpanmishra@google.com>
2024-03-24 17:53:18 +0530

    chore: increase reads to 12000.

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/AbstractLatencyBenchmark.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/AbstractLatencyBenchmark.java
@@ -40,7 +40,7 @@ public abstract class AbstractLatencyBenchmark {
    * duration of the benchmark. For ex - With PARALLEL_THREADS = 2, TOTAL_READS_PER_RUN = 200, there
    * will be 400 read requests (200 on each thread).
    */
-  static final int TOTAL_READS_PER_RUN = 1;
+  static final int TOTAL_READS_PER_RUN = 12000;
 
   /**
    * Number of requests which are used to initialise/warmup the benchmark. The latency number of
