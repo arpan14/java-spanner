de3b700f0b2418aa3f2b4ad01e55ffad3a036f67
Arpan Mishra <arpanmishra@google.com>
2024-04-23 23:16:54 +0530

    chore: adding a benchmark utility in client.

---
+++ benchmarks/README.md
@@ -0,0 +1,8 @@
+## Running
+
+The benchmark application includes PGAdapter as a dependency and automatically starts PGAdapter as
+an in-process service.
+
+```shell
+mvn clean compile exec:java -Dexec.args="--clients=16 --operations=1000"
+```
---
+++ benchmarks/src/main/java/com/google/cloud/spanner/benchmark/BenchmarkRunner.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spanner.benchmark;
+
+import java.time.Duration;
+import java.util.List;
+
+public interface BenchmarkRunner {
+  enum TransactionType {
+    READ_ONLY {
+      @Override
+      String getJdbcSql() {
+        return "select col_varchar from latency_test where col_bigint=?";
+      }
+
+      @Override
+      String getSql() {
+        return "select col_varchar from latency_test where col_bigint=$1";
+      }
+    },
+    READ_WRITE {
+      @Override
+      String getJdbcSql() {
+        return "update latency_test set col_varchar=? where col_bigint=?";
+      }
+
+      @Override
+      String getSql() {
+        return "update latency_test set col_varchar=$1 where col_bigint=$2";
+      }
+    };
+
+    abstract String getJdbcSql();
+
+    abstract String getSql();
+  }
+
+  List<Duration> execute(
+      TransactionType transactionType,
+      int numClients,
+      int numOperations,
+      int waitMillis,
+      boolean useMultiplexedSession);
+}
